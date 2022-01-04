774b48802288a49d9fdb3bb25717a1470e141be0
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2022-01-04 23:00:54 +0100

    update project

--- app/src/main/java/com/dev_mobile/miniprojet_android/Accueil.java
+++ app/src/main/java/com/dev_mobile/miniprojet_android/Accueil.java
@@ -125,7 +125,7 @@ public class Accueil extends AppCompatActivity {
     {
         FirebaseRecyclerOptions<Movie> options =
                 new FirebaseRecyclerOptions.Builder<Movie>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("movie").orderByChild("marque").startAt(s).endAt(s+"\uf8ff"), Movie.class)
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("movie").orderByChild("titre").startAt(s).endAt(s+"\uf8ff"), Movie.class)
                         .build();
 
         adapter=new myadapter(options,getApplicationContext());
